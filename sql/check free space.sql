cf79601e1fe53769d2fdc75b20619d040ec37fb4
ajaysinghrathore1 <ajaysinghrathore1@gmail.com>
2025-06-03 15:01:15 +0530

    Create check free space.sql

---
+++ sql/check free space.sql
@@ -0,0 +1,18 @@
+SELECT DB_NAME(database_id) AS database_name, 
+    type_desc, 
+    name AS FileName, 
+    size/128.0 AS CurrentSizeMB
+FROM sys.master_files
+WHERE 
+DB_NAME(database_id)='SSISDB'
+--database_id > 6 AND type IN (0,1)
+
+
+SELECT DB_NAME() AS DbName, 
+    name AS FileName, 
+    type_desc,
+    cast((size/128.0)/1000  as numeric(15,2)) AS CurrentSizeGB,  
+    cast((size/128.0)/1000 - (CAST(FILEPROPERTY(name, 'SpaceUsed') AS INT)/128.0)/1000  as numeric(15,2)) AS FreeSpaceGB
+FROM sys.database_files
+WHERE 
+DB_NAME()='SSISDB'
